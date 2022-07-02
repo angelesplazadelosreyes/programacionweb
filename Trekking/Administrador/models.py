484c832d52a91d0440654c1b1666e2547c8d5d8e
angelesplazadelosreyes <mplazadelosreyes@gmail.com>
2022-07-02 08:14:53 -0400

    8:14 am

--- Trekking/Administrador/models.py
+++ Trekking/Administrador/models.py
@@ -5,8 +5,8 @@ from Publico.views import rutas
 # Create your models here.
 
 class Usuario(models.Model):
-    id=models.IntegerField
-    idPerfil=models.IntegerField
+    id=models.IntegerField(primary_key=True)
+    idPerfil=models.IntegerField()
     correo=models.EmailField(max_length = 200)
     rut=models.CharField(max_length = 10)
     nombres=models.CharField(max_length = 100)
@@ -23,35 +23,35 @@ class Usuario(models.Model):
 
 
 class EstadoSalud(models.Model):
-    id=models.IntegerField
-    idUsuario=id=models.IntegerField
-    peso=models.IntegerField
-    estatura=models.IntegerField
-    probCardiaco=models.CharField
-    probPresion=models.CharField
-    movilidad=models.CharField
-    otrosPob=models.CharField
+    id=models.IntegerField(primary_key=True)
+    idUsuario=models.ForeignKey(Usuario)
+    peso=models.IntegerField()
+    estatura=models.IntegerField()
+    probCardiaco=models.CharField(max_length = 100)
+    probPresion=models.CharField(max_length = 100)
+    movilidad=models.CharField(max_length = 100)
+    otrosPob=models.CharField(max_length = 100)
 
 
 class Perfil(models.Model):
-    id=models.IntegerField
-    tipo=models.CharField
+    id=models.IntegerField(primary_key=True)
+    tipo=models.CharField(max_length = 10)
 
 
 class Acceso(models.Model):
-    id=models.IntegerField
-    idPerfil=models.IntegerField
-    idMenu=models.IntegerField
+    id=models.IntegerField(primary_key=True)
+    idPerfil=models.ForeignKey(Perfil)
+    idMenu=models.IntegerField(max_length = 10)
 
 
 class Menu(models.Model):
-    id=models.IntegerField
-    nombre=models.CharField
-    url=models.CharField
+    id=models.IntegerField(primary_key=True)
+    nombre=models.CharField(max_length = 100)
+    url=models.CharField(max_length = 200)
 
 
 class Mensaje(models.Model):
-    id=models.IntegerField
+    id=models.IntegerField(primary_key=True)
     idUsuario=models.IntegerField
     tipo=models.CharField
     fecha=models.DateField(auto_now_add=True)
@@ -62,49 +62,49 @@ class Mensaje(models.Model):
 
 
 class Respuesta(models.Model):
-    id=models.IntegerField
+    id=models.IntegerField(primary_key=True)
     idMensaje=models.IntegerField
     fecha=models.DateField(auto_now_add=True)
-    titulo=models.CharField
-    cuerpo=models.TextField
-    estado=models.CharField
-    tipo=models.CharField
+    titulo=models.CharField(max_length = 100)
+    cuerpo=models.TextField(max_length = 1000)
+    estado=models.CharField(max_length = 10)
+    tipo=models.CharField(max_length = 10)
 
 
 class Noticia(models.Model):
-    id=models.IntegerField
+    id=models.IntegerField(primary_key=True)
     fecha=models.DateField(auto_now_add=True)
-    titulo=models.CharField
-    bajada=models.CharField
-    cuerpo=models.TextField
+    titulo=models.CharField(max_length = 100)
+    bajada=models.CharField(max_length = 400)
+    cuerpo=models.TextField(max_length = 1000)
     imagen=models.ImageField
-    estado=models.CharField
-    prioridad=models.CharField
+    estado=models.CharField(max_length = 10)
+    prioridad=models.CharField(max_length = 10)
 
 
 class Galeria(models.Model):
-    id=models.IntegerField
-    titulo=models.CharField
-    url=models.CharField
+    id=models.IntegerField(primary_key=True)
+    titulo=models.CharField(max_length = 100)
+    url=models.CharField(max_length = 200)
     fecha=models.DateField(auto_now_add=True)
-    estado=models.CharField
-    prioridad=models.CharField
-    idPerfil=models.IntegerField
+    estado=models.CharField(max_length = 10)
+    prioridad=models.CharField(max_length = 10)
+    idPerfil=models.ForeignKey(Perfil)
 
 
 class Ruta(models.Model):
-    id=models.IntegerField
-    titulo=models.CharField
-    descripcion=models.CharField
-    dificultad=models.CharField
+    id=models.IntegerField(primary_key=True)
+    titulo=models.CharField(max_length = 100)
+    descripcion=models.CharField(max_length = 400)
+    dificultad=models.CharField(max_length = 10)
     distancia=models.IntegerField
     tiempo=models.DurationField
-    urlMap=models.CharField
+    urlMap=models.CharField(max_length = 200)
     fechaActualizacion=models.DateField
 
 
 class Salida(models.Model):
-    id=models.IntegerField
+    id=models.IntegerField(primary_key=True)
     idRuta=models.IntegerField
     horaInicio=models.DateTimeField
     precio=models.IntegerField
@@ -112,26 +112,26 @@ class Salida(models.Model):
     cupoUsado=models.IntegerField
     cupoTraslado=models.IntegerField
     cupoTrasladoUsado=models.IntegerField
-    estado=models.CharField
+    estado=models.CharField(max_length = 10)
     fecha=models.DateField
-    lugarEncuentro=models.CharField
+    lugarEncuentro=models.CharField(max_length = 100)
 
 
 class Inscripcion(models.Model):
-    id=models.IntegerField
+    id=models.IntegerField(primary_key=True)
     idUsuario=models.IntegerField
     idSalida=models.IntegerField
-    estadoPago=models.CharField
+    estadoPago=models.CharField(max_length = 20)
     fechaPago=models.DateField
-    tipoPago=models.CharField
-    codigoTb=models.CharField
-    estado=models.CharField
+    tipoPago=models.CharField(max_length = 20)
+    codigoTb=models.CharField(max_length = 50)
+    estado=models.CharField(max_length = 10)
     tipoDevolucion=models.CharField
 
 
 class Faq(models.Model):
-    id=models.IntegerField
-    titulo=models.CharField
-    cuerpo=models.TextField
+    id=models.IntegerField(primary_key=True)
+    titulo=models.CharField(max_length = 100)
+    cuerpo=models.TextField(max_length = 400)
     fecha=models.DateField
-    estado=models.CharField
+    estado=models.CharField(max_length = 10)
